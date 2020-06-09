b2e6eebf9170d67f123a4ddb46c03857f4d5d861
Michael Walker <mike@barrucadu.co.uk>
2020-06-09 15:27:59 +0100

    Create a token to validate the account

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,3 +1,5 @@
+require "base64"
+
 class RegisterController < ApplicationController
   # bad!
   skip_before_action :verify_authenticity_token
@@ -8,13 +10,17 @@ class RegisterController < ApplicationController
 
   def create
     @email = params[:email]
-    Services.keycloak.users.create!(
+    user = Services.keycloak.users.create!(
       @email,
       @email,
       params[:password],
       false,
       "en",
     )
+    magic_value = SecureRandom.hex(64)
+    rep = { "attributes" => { "verification_token" => magic_value, "verification_token_expires" => Time.zone.now + 24.hours } }
+    Services.keycloak.users.update(user.id, KeycloakAdmin::UserRepresentation.from_hash(rep))
+    @token = Base64.urlsafe_encode64("#{user.id}\0#{magic_value}")
   end
 
   def conflict
