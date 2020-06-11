368eda008df314bcfc4026800fa8ecf3ac2de135
Michael Walker <mike@barrucadu.co.uk>
2020-06-11 09:45:55 +0100

    Display a new message if the user has already verified their email

--- app/controllers/verify_controller.rb
+++ app/controllers/verify_controller.rb
@@ -4,7 +4,7 @@ class VerifyController < ApplicationController
     token = params.fetch(:token)
 
     user = Services.keycloak.users.get(@user_id)
-    @state = EmailConfirmation.check_and_verify(user, token)
+    @state = user.email_verified ? :already_verified : EmailConfirmation.check_and_verify(user, token)
   rescue KeyError
     @state = :bad_parameters
   end
