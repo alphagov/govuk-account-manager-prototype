32d5b3bf16aac22fd9b586e9b1380f88c47502d5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-06-15 14:18:41 +0100

    Implement password confirmation for registration form

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,22 +1,39 @@
 class RegisterController < ApplicationController
+  include PasswordHelper
   rescue_from RestClient::Conflict, with: :conflict
 
   def show; end
 
   def create
-    @email = params[:email]
-    user = Services.keycloak.users.create!(
-      @email,
-      @email,
-      params[:password],
-      false,
-      "en",
-    )
+    @email = register_params[:email]
 
-    EmailConfirmation.send(user)
+    password_validity = password_valid?(register_params[:password], register_params[:password_confirm])
+
+    if password_validity == :ok
+      user = Services.keycloak.users.create!(
+        @email,
+        @email,
+        params[:password],
+        false,
+        "en",
+      )
+
+      EmailConfirmation.send(user)
+    else
+      flash[:validation] = [{
+        field: "password",
+        text: t("register.create.error.#{password_validity}"),
+      }]
+
+      redirect_to action: :show
+    end
   end
 
 private
+
+  def register_params
+    params.permit(:email, :password, :password_confirm)
+  end
 
   def conflict
     @email = params[:email]
