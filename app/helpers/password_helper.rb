74c8479b834cebf829d3ec788045cc4c0d8f2626
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-06-17 10:12:04 +0100

    Update password validation helper to return all errors
    We were previously only returning the first error encountered, but it is
    more useful to present users with all the errors on that page.
    Therefore returning an array of errors, rather than a single value.

--- app/helpers/password_helper.rb
+++ app/helpers/password_helper.rb
@@ -1,14 +1,13 @@
 module PasswordHelper
   def password_valid?(password, password_confirm)
-    return :password_missing if password.blank?
+    errors = []
 
-    return :password_confirm_missing if password_confirm.blank?
+    errors << :password_missing if password.blank?
+    errors << :password_confirm_missing if password_confirm.blank?
+    errors << :password_mismatch if password.present? && password_confirm.present? && password != password_confirm
+    errors << :password_invalid if password.present? && !password_meets_criteria?(password)
 
-    return :password_mismatch unless password == password_confirm
-
-    return :password_invalid unless password_meets_criteria?(password)
-
-    :ok
+    errors
   end
 
 private
--- spec/helpers/password_helper_spec.rb
+++ spec/helpers/password_helper_spec.rb
@@ -1,23 +1,23 @@
 RSpec.describe PasswordHelper, type: :helper do
   describe "#password_valid?" do
     it "returns an error if first password is missing" do
-      expect(password_valid?("", "foo")).to eq(:password_missing)
+      expect(password_valid?("", "foo")).to include(:password_missing)
     end
 
     it "returns an error if second password is missing" do
-      expect(password_valid?("foo", "")).to eq(:password_confirm_missing)
+      expect(password_valid?("foo", "")).to include(:password_confirm_missing)
     end
 
     it "returns an error if passwords do not match" do
-      expect(password_valid?("foo1", "foo2")).to eq(:password_mismatch)
+      expect(password_valid?("foo1", "foo2")).to include(:password_mismatch)
     end
 
     it "returns an error if password is less than 8 characters" do
-      expect(password_valid?("abc1234", "abc1234")).to eq(:password_invalid)
+      expect(password_valid?("abc1234", "abc1234")).to include(:password_invalid)
     end
 
-    it "returns an error if password does not contain a number" do
-      expect(password_valid?("abcdefghj", "abcdefghj")).to eq(:password_invalid)
+    it "returns an error if password does not include a number" do
+      expect(password_valid?("abcdefghj", "abcdefghj")).to include(:password_invalid)
     end
   end
 end
