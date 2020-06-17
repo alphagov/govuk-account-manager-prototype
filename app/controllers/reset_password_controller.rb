e9530a1f1c53ab0a5f755e4bf1f48f1e814990e0
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-06-17 11:35:42 +0100

    Implement validation for password reset page

--- app/controllers/reset_password_controller.rb
+++ app/controllers/reset_password_controller.rb
@@ -1,13 +1,20 @@
 require "reset_password"
 
 class ResetPasswordController < ApplicationController
-  def show; end
+  def show
+    @email = reset_password_params[:email]
+  end
 
   def submit
     @email = reset_password_params[:email]
     user = Services.keycloak.users.search(@email)
     if user.empty?
-      @state = :no_such_user
+      flash[:validation] = [{
+        field: "email",
+        text: t("reset_password.no_such_user"),
+      }]
+
+      redirect_to action: :show, params: reset_password_params
     else
       ResetPassword.send(user.first)
       user = Services.keycloak.users.search(@email)
