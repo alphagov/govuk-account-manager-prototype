985b410f61a7c36fa01ff1648e757d8737ca388e
Huw Diprose <mail@huwdiprose.co.uk>
2020-06-23 16:43:13 +0100

    Change paths from /manage to /account/manage

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,6 +2,6 @@ class CallbackController < ApplicationController
   def show
     result = Services.oidc.handle_redirect(params[:code], session[:nonce])
     session[:sub] = result[:user_info].sub
-    redirect_to "/manage"
+    redirect_to "/account/manage"
   end
 end
