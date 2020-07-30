c5ecee9c635579404bc82e3037d62f37decbf823
Huw Diprose <huw.diprose@digital.cabinet-office.co.uk>
2020-07-30 16:53:10 +0100

    Add Factory data for :oauth_application
    Add basic stuff for :oauth_application, and a nexted version specific to Email Alert API read only

--- spec/factories/doorkeeper.rb
+++ spec/factories/doorkeeper.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :oauth_application, class: Doorkeeper::Application do
+    id            { 1 }
+    uid           { "8AGx4q2ltTBJU0KPlbOzjdWsmm3k80VVNkKjwGkcS1U" }
+    secret        { "SlRdBYZV7TB7PbktkQnHibXvyv2g2uNj3XeQjSa0gJA" }
+    confidential  { true }
+
+    factory :email_alert_api, class: Doorkeeper::Application do
+      name          { "Email Alert API" }
+      redirect_uri  { "http://localhost:3000/auth/email-subscriptions/callback" }
+      scopes        { :"emailsubscriptions.read_only" }
+    end
   end
 
   factory :oauth_access_token, class: Doorkeeper::AccessToken do
