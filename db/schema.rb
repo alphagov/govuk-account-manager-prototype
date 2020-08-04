de1ed3824a028efb5432024ebdd736684ed2e875
Michael Walker <mike@barrucadu.co.uk>
2020-08-04 12:48:22 +0100

    Add a unique index on OAuth application names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_09_155309) do
+ActiveRecord::Schema.define(version: 2020_08_04_110328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2020_07_09_155309) do
     t.boolean "confidential", default: true, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_oauth_applications_on_name", unique: true
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
   end
 
