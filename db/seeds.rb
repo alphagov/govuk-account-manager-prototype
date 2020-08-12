36db23da7a5e13cb0783ecc99e70b1b4dfd7571c
Michael Walker <mike@barrucadu.co.uk>
2020-08-12 15:29:24 +0100

    Add 'Apply for a Barking Permit' app to db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,4 +15,12 @@ unless Rails.env.production?
 
   token.token = "attribute-service-token"
   token.save!
+
+  Doorkeeper::Application.create!(
+    name: "Apply for a Barking Permit",
+    redirect_uri: "http://apply-for-a-barking-permit.service.dev.gov.uk/login/callback",
+    scopes: %i[email test_scope_read openid],
+    uid: "barking-permit-id",
+    secret: "barking-permit-secret",
+  )
 end
