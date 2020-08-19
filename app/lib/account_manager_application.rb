1a0e5a937a729d8261b5dfa18716559d71c6075e
Michael Walker <mike@barrucadu.co.uk>
2020-08-19 12:02:23 +0100

    Move token logic from RemoteUserInfo to AccountManagerApplication

--- app/lib/account_manager_application.rb
+++ app/lib/account_manager_application.rb
@@ -3,30 +3,51 @@ class AccountManagerApplication
   REDIRECT_URI = Rails.application.config.redirect_base_url
   SCOPES = %i[account_manager_access].freeze
 
-  def self.fetch
-    new.fetch
-  end
-
-  def fetch
-    @fetch ||= Doorkeeper::Application.transaction do
+  def self.application
+    Doorkeeper::Application.transaction do
       application = find_application
       application.nil? ? create_application : application
     end
   end
 
-private
-
-  def find_application
-    Doorkeeper::Application.find_by(name: NAME)
-  rescue ActiveRecord::RecordNotFound
-    nil
+  def self.user_token(user_id)
+    Doorkeeper::AccessToken.transaction do
+      token = find_token(user_id)
+      token.nil? ? create_token(user_id) : token
+    end
   end
 
-  def create_application
-    Doorkeeper::Application.create!(
-      name: NAME,
-      redirect_uri: REDIRECT_URI,
-      scopes: SCOPES,
-    )
+  class << self
+  private
+
+    def find_application
+      Doorkeeper::Application.find_by(name: NAME)
+    rescue ActiveRecord::RecordNotFound
+      nil
+    end
+
+    def create_application
+      Doorkeeper::Application.create!(
+        name: NAME,
+        redirect_uri: REDIRECT_URI,
+        scopes: SCOPES,
+      )
+    end
+
+    def find_token(user_id)
+      Doorkeeper::AccessToken.matching_token_for(
+        application,
+        user_id,
+        Doorkeeper::OAuth::Scopes.from_array(SCOPES),
+      )
+    end
+
+    def create_token(user_id)
+      Doorkeeper::AccessToken.create!(
+        application_id: application.id,
+        resource_owner_id: user_id,
+        scopes: SCOPES,
+      )
+    end
   end
 end
--- app/lib/remote_user_info.rb
+++ app/lib/remote_user_info.rb
@@ -1,6 +1,4 @@
 class RemoteUserInfo
-  TOKEN_SCOPES = %i[account_manager_access].freeze
-
   def self.call(user)
     new(user).user_info
   end
@@ -32,28 +30,6 @@ class RemoteUserInfo
   end
 
   def token
-    @token ||= Doorkeeper::AccessToken.transaction do
-      application = AccountManagerApplication.fetch
-      token = find_token(application)
-      token.nil? ? create_token(application) : token
-    end
-  end
-
-private
-
-  def find_token(application)
-    Doorkeeper::AccessToken.matching_token_for(
-      application,
-      @user.id,
-      Doorkeeper::OAuth::Scopes.from_array(TOKEN_SCOPES),
-    )
-  end
-
-  def create_token(application)
-    Doorkeeper::AccessToken.create!(
-      application_id: application.id,
-      resource_owner_id: @user.id,
-      scopes: TOKEN_SCOPES,
-    )
+    @token ||= AccountManagerApplication.user_token(@user.id)
   end
 end
