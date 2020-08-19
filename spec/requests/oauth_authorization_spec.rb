e818910ed6eccc30643acc0738d41eb18f9e8465
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-08-19 11:51:07 +0100

    Add test for authorization flow
    We have added some special behaviour to the authorization flow around
    displaying the permissions required, so we should have a test that
    covers this.

---
+++ spec/requests/oauth_authorization_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+RSpec.feature "/oauth/authorize" do
+  let(:user) do
+    FactoryBot.create(
+      :user,
+      email: "user@domain.tld",
+      password: "breadbread1",
+      password_confirmation: "breadbread1",
+    )
+  end
+
+  let(:application) do
+    FactoryBot.create(
+      :oauth_application,
+      name: "Some Other Government Service",
+      redirect_uri: "https://www.gov.uk",
+      scopes: %i[openid email transition_checker],
+    )
+  end
+
+  context "with a user logged in" do
+    before do
+      log_in(user.email, user.password)
+    end
+
+    it "asks for authorization to access listed scopes" do
+      visit authorization_endpoint_url(client: application, scope: "openid email")
+
+      expect(page).to have_text(I18n.t("doorkeeper.scopes.openid"))
+      expect(page).to have_text(I18n.t("doorkeeper.scopes.email"))
+    end
+
+    it "does not ask for authorization to access transition checker state" do
+      visit authorization_endpoint_url(client: application, scope: "openid email transition_checker")
+
+      expect(page).not_to have_text(I18n.t("doorkeeper.scopes.transition_checker"))
+    end
+  end
+end
---
+++ spec/support/helpers/url_helper.rb
@@ -0,0 +1,16 @@
+module UrlHelper
+  def authorization_endpoint_url(options = {})
+    parameters = {
+      client_id: options[:client_id] || options[:client].try(:uid),
+      redirect_uri: options[:redirect_uri] || options[:client].try(:redirect_uri),
+      response_type: options[:response_type] || "code",
+      scope: options[:scope],
+      state: options[:state],
+      code_challenge: options[:code_challenge],
+      code_challenge_method: options[:code_challenge_method],
+    }.reject { |_, v| v.blank? }
+    "/oauth/authorize?#{parameters.to_query}"
+  end
+end
+
+RSpec.configuration.send :include, UrlHelper
