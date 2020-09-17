4e5f2b3fd41bdcb4ba30f98ff2c77d9d2174f921
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-09-17 11:55:25 +0100

    Add helper to present non-Devise errors

--- app/helpers/error_items_helper.rb
+++ app/helpers/error_items_helper.rb
@@ -1,4 +1,13 @@
 module ErrorItemsHelper
+  def error_items(field)
+    if flash[:validation] && flash[:validation].select { |item| item[:field] == field }.any?
+      sanitize(flash[:validation]
+        .select { |key| key.to_s.match(field) }
+        .map { |error| error[:text] }.uniq
+        .join("<br>"))
+    end
+  end
+
   def devise_error_items(field)
     return nil unless resource
 
