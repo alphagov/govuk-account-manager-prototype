d22a0fbeafb7df4aafde1af9e472bb206fe7b929
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-09-17 13:23:00 +0100

    Show error when required fields in feedback form not entered

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,3 +1,17 @@
 class FeedbackController < ApplicationController
+  REQUIRED_FIELDS = %w[comments email response_required].freeze
+
   def show; end
+
+  def submit
+    errors = []
+    REQUIRED_FIELDS.each do |field|
+      errors << { field: field, text: I18n.t("feedback.fields.#{field}.not_present_error") } if params[field.to_sym].blank?
+    end
+
+    unless errors.empty?
+      flash.now[:validation] = errors
+      render "show"
+    end
+  end
 end
--- spec/requests/feedback_spec.rb
+++ spec/requests/feedback_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe "feedback" do
       expect(response.body).to have_content(I18n.t("feedback.title"))
     end
   end
+
+  describe "POST" do
+    let(:params) do
+      {
+        "email" => "user@digital.cabinet-office.gov.uk",
+        "comments" => "This website is amazing",
+        "response_required" => "yes",
+      }
+    end
+
+    %w[comments email response_required].each do |field|
+      it "shows an error when required field #{field} is missing" do
+        post feedback_path, params: params.except(field)
+
+        expect(response.body).to have_content(I18n.t("feedback.fields.#{field}.not_present_error"))
+      end
+    end
+  end
 end
