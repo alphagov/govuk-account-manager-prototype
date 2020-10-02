bbe99be18599a21e3543ed7803018a2e809fbaeb
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-10-02 13:35:45 +0100

    Only require name and email when user wants a response
    We are using the feedback form to collect both anonymous and
    personalised feedback, so should therefore only require contact details
    if the user wants a response.

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,5 +1,5 @@
 class FeedbackController < ApplicationController
-  REQUIRED_FIELDS = %w[comments name email user_requires_response].freeze
+  REQUIRED_FIELDS = %w[comments user_requires_response].freeze
 
   def show
     @form_responses = {}
@@ -7,14 +7,19 @@ class FeedbackController < ApplicationController
 
   def submit
     @form_responses = {
-      name: strip_tags(params[:name]).presence,
-      email: strip_tags(params[:email]).presence,
       comments: strip_tags(params[:comments]).presence,
       user_requires_response: strip_tags(params[:user_requires_response]).presence,
     }
 
+    conditionally_required_fields = []
+    if @form_responses[:user_requires_response] == "yes"
+      conditionally_required_fields = %w[name email]
+      @form_responses[:name] = strip_tags(params[:name]).presence
+      @form_responses[:email] = strip_tags(params[:email]).presence
+    end
+
     errors = []
-    REQUIRED_FIELDS.each do |field|
+    (REQUIRED_FIELDS + conditionally_required_fields).each do |field|
       errors << { field: field, text: I18n.t("feedback.fields.#{field}.not_present_error") } if @form_responses[field.to_sym].blank?
     end
 
@@ -25,8 +30,8 @@ class FeedbackController < ApplicationController
 
     ticket_attributes = {
       subject: I18n.t("feedback.email_subject"),
-      name: @form_responses[:name],
-      email: @form_responses[:email],
+      name: @form_responses.fetch(:name, I18n.t("feedback.anonymous_name")),
+      email: @form_responses.fetch(:email, I18n.t("feedback.anonymous_email")),
       comments: @form_responses[:comments],
       user_requires_response: @form_responses[:user_requires_response].humanize,
     }
