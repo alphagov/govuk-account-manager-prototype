f374f8680b96b467bd976d29b70b8abbca18da5e
Michael Walker <mike@barrucadu.co.uk>
2020-10-08 14:57:52 +0100

    Fix an error in the SetAttributesJob

--- app/jobs/set_attributes_job.rb
+++ app/jobs/set_attributes_job.rb
@@ -2,7 +2,7 @@ class SetAttributesJob < ApplicationJob
   queue_as :default
 
   def perform(access_token_id, attributes)
-    token = Doorkeeper::AccessToken.find(access_token_id).token
+    token = Doorkeeper::AccessToken.find(access_token_id)
     attributes.each do |key, value|
       RestClient.put(
         "#{ENV['ATTRIBUTE_SERVICE_URL']}/v1/attributes/#{key}",
