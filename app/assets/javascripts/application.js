a40dfb30920fe0f8739e4bd3b7b6dee4f734f7b3
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-10-13 17:13:48 +0100

    Update styles and layout
    - use individual component assets
    - use components for header and footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,5 @@
-//= require govuk_publishing_components/dependencies
-//= require govuk_publishing_components/all_components
+//= require govuk_publishing_components/lib
+//= require govuk_publishing_components/components/button
+//= require govuk_publishing_components/components/checkboxes
+//= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/radio
