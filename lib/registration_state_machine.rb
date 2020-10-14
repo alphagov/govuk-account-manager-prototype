c29eb94246bb4b1bc1fe38662f25f1cf064fcd3a
Michael Walker <mike@barrucadu.co.uk>
2020-10-14 16:55:20 +0100

    Split up registration state machine

--- lib/registration_state_machine.rb
+++ lib/registration_state_machine.rb
@@ -1,50 +1,61 @@
 module RegistrationStateMachine
   def self.call(params, jwt_payload)
-    state = nil
-    resource_error_messages = nil
+    password_errors = check_password(params)
+    email_decision_errors = check_email_decision(params, jwt_payload)
 
+    if password_errors
+      {
+        state: :needs_password,
+        resource_error_messages: password_errors,
+      }
+    elsif email_decision_errors
+      {
+        state: :needs_email_decision,
+        resource_error_messages: email_decision_errors,
+      }
+    else
+      {
+        state: :finish,
+        resource_error_messages: nil,
+      }
+    end
+  end
+
+  def self.check_password(params)
     password = params.dig(:user, :password) # pragma: allowlist secret
     password_confirmation = params.dig(:user, :password_confirmation)
-    if password
-      password_format_ok = User::PASSWORD_REGEX.match? password
-      password_length_ok = Devise.password_length.include? password.length
-      password_confirmation_ok = password == password_confirmation
+    return {} unless password
 
-      if password_format_ok && password_length_ok && password_confirmation_ok
-        email_topic_slug = (jwt_payload || {}).dig(:attributes, :transition_checker_state, "email_topic_slug")
-
-        if email_topic_slug
-          email_decision = params.dig(:email_decision)
-          email_decision_format_ok = %w[yes no].include? email_decision
-
-          if email_decision && email_decision_format_ok
-            state = :finish
-          else
-            state = :needs_email_decision
-            resource_error_messages = {
-              email_decision: email_decision ? [I18n.t("activerecord.errors.models.user.attributes.email_decision.invalid")] : nil,
-            }.compact
-          end
-        else
-          state = :finish
-        end
-      else
-        state = :needs_password
-        resource_error_messages = {
-          password: [ # pragma: allowlist secret
-            password_format_ok ? nil : I18n.t("activerecord.errors.models.user.attributes.password.invalid"),
-            password_length_ok ? nil : I18n.t("activerecord.errors.models.user.attributes.password.too_short"),
-          ],
-          password_confirmation: password_confirmation_ok ? nil : [I18n.t("activerecord.errors.models.user.attributes.password_confirmation.confirmation")],
-        }.compact
-      end
-    else
-      state = :needs_password
+    password_format_ok = User::PASSWORD_REGEX.match? password
+    password_length_ok = Devise.password_length.include? password.length
+    password_confirmation_ok = password == password_confirmation
+    unless password_format_ok && password_length_ok && password_confirmation_ok
+      {
+        password: [ # pragma: allowlist secret
+          password_format_ok ? nil : I18n.t("activerecord.errors.models.user.attributes.password.invalid"),
+          password_length_ok ? nil : I18n.t("activerecord.errors.models.user.attributes.password.too_short"),
+        ],
+        password_confirmation: [
+          password_confirmation_ok ? nil : I18n.t("activerecord.errors.models.user.attributes.password_confirmation.confirmation"),
+        ],
+      }
     end
+  end
 
-    {
-      state: state,
-      resource_error_messages: resource_error_messages,
-    }
+  def self.check_email_decision(params, jwt_payload)
+    email_topic_slug = (jwt_payload || {}).dig(:attributes, :transition_checker_state, "email_topic_slug")
+    if email_topic_slug
+      email_decision = params.dig(:email_decision)
+      return {} unless email_decision
+
+      email_decision_format_ok = %w[yes no].include? email_decision
+      unless email_decision_format_ok
+        {
+          email_decision: [
+            I18n.t("activerecord.errors.models.user.attributes.email_decision.invalid"),
+          ],
+        }
+      end
+    end
   end
 end
