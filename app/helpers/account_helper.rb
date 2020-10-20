0db1f017785273f51d67127dc09f4204f06f5044
Michael Walker <mike@barrucadu.co.uk>
2020-10-20 16:53:20 +0100

    Use Plek.new.website_root to link to transition checker

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,11 +1,10 @@
 module AccountHelper
   def finder_frontend_base_uri
-    redirect_uri = Doorkeeper::Application.find_by(name: "Transition Checker").redirect_uri
-    URI(redirect_uri).tap { |u| u.path = "" }.to_s
+    Plek.new.website_root
   end
 
   def email_alert_frontend_base_uri
-    finder_frontend_base_uri.sub("finder-frontend", "email-alert-frontend")
+    Plek.new.website_root
   end
 
   def paths_without_feedback_footer
