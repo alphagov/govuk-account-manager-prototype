640169266676f96392514331d82d6cb9441208b1
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-10-21 10:05:53 +0100

    Add new delete confirmation route and controller
    It's embedded in the existing delete controller as an unauthenticated
    route

--- app/controllers/delete_controller.rb
+++ app/controllers/delete_controller.rb
@@ -1,5 +1,7 @@
 class DeleteController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:show]
 
   def show; end
+
+  def confirmation; end
 end
