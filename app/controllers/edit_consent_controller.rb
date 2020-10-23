9e676498efe0000bfd4056446750712e215daafd
Michael Walker <mike@barrucadu.co.uk>
2020-10-23 16:55:24 +0100

    Implement edit cookie consent page

--- app/controllers/edit_consent_controller.rb
+++ app/controllers/edit_consent_controller.rb
@@ -1,6 +1,13 @@
 class EditConsentController < ApplicationController
   before_action :authenticate_user!
 
+  def cookie; end
+
+  def cookie_send
+    current_user.update!(cookie_consent: params[:cookie_consent] == "yes")
+    redirect_to(account_manage_path)
+  end
+
   def feedback; end
 
   def feedback_send
