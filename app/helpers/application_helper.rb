1e2d5d84a28380e54e1ecf702713b555088b15af
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-10-26 13:10:33 +0000

    Update content for MFA phone number create
    Requires a new bottom link that allows the user to bail out and get back
    to the email alerts only path. As this is a complex URL and needs to be
    plucked out of the JWT I've put this in a helper.
    
    Also added a new class to the form and put a margin-bottom on.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,18 @@ module ApplicationHelper
       []
     end
   end
+
+  def has_criteria_keys?(registration_state)
+    return false if registration_state.blank?
+
+    registration_state.jwt_payload&.dig("attributes", "transition_checker_state", "criteria_keys").present?
+  end
+
+  def email_alerts_only_path(registration_state)
+    checker_results = registration_state.jwt_payload&.dig("attributes", "transition_checker_state", "criteria_keys")
+    email_signup_path = "/transition-check/email-signup"
+    checker_results = "?c[]=" + checker_results.join('.join("&c[]=")')
+    base_url = Rails.env.development? ? Plek.find("finder-frontend") : Plek.website_root
+    URI.join(base_url, email_signup_path, checker_results)
+  end
 end
