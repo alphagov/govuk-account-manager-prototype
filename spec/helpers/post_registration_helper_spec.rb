963d18e34541f5da7f8b2386df8a67c9693e4fec
Michael Walker <mike@barrucadu.co.uk>
2020-10-26 12:34:36 +0000

    Pass cookie consent back to the transition checker
    This is a hacky solution which works for now but which we will want to
    revisit soon: if the user has consented to cookies *and* the
    previous_url takes them back to the transition checker, append
    something to the state.
    
    This is not good because it couples us to the representation of the
    transition checker state, and will also break if the query params get
    re-ordered.
    
    A better solution would be storing cookie consent as an attribute, but
    currently we only want to implement per-session consent, not
    per-account consent, so that doesn't work.

--- spec/helpers/post_registration_helper_spec.rb
+++ spec/helpers/post_registration_helper_spec.rb
@@ -1,5 +1,7 @@
 RSpec.describe PostRegistrationHelper, type: :helper do
-  describe "#service_name_for" do
+  describe "#service_for" do
+    let(:user) { FactoryBot.create(:user) }
+
     let(:application) do
       FactoryBot.create(
         :oauth_application,
@@ -11,18 +13,18 @@ RSpec.describe PostRegistrationHelper, type: :helper do
 
     it "extracts the service name using the client_id parameter" do
       url = oauth_authorization_path + "?" + Rack::Utils.build_nested_query(client_id: application.uid)
-      expect(service_name_for(url)).to eq(application.name)
+      expect(service_for(url, user)[:name]).to eq(application.name)
     end
 
     it "only produces a service name if the link looks like an OAuth content URL" do
       url = "//nefarious-attempt-to-embed-an-arbitrary-link?" + Rack::Utils.build_nested_query(client_id: application.uid)
-      expect(service_name_for(url)).to be_nil
+      expect(service_for(url, user)).to be_nil
     end
 
     context "the client_id doesn't match an application" do
       it "returns nil" do
         url = oauth_authorization_path + "?" + Rack::Utils.build_nested_query(client_id: "breadbread")
-        expect(service_name_for(url)).to be_nil
+        expect(service_for(url, user)).to be_nil
       end
     end
   end
