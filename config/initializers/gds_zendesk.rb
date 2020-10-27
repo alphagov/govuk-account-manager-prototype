a6eb800a810097d350c7d4d427ea47f05ba81065
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-10-27 10:21:30 +0000

    Remove anonymous ticket constant
    This was generated by the gds_zendesk gem, but we're not using it in
    this application.

--- config/initializers/gds_zendesk.rb
+++ config/initializers/gds_zendesk.rb
@@ -2,8 +2,6 @@ require "yaml"
 require "gds_zendesk/client"
 require "gds_zendesk/dummy_client"
 
-ZENDESK_ANONYMOUS_TICKETS_REQUESTER_EMAIL = ENV["ZENDESK_ANONYMOUS_TICKET_EMAIL"] || "api-user@example.com"
-
 GDS_ZENDESK_CLIENT = if Rails.env.development?
                        GDSZendesk::DummyClient.new(logger: Rails.logger)
                      else
