0bcf8cce3777ebd9871f2b7492301d5057d92ba6
Michael Walker <mike@barrucadu.co.uk>
2020-10-30 09:17:03 +0000

    Implement account-manager side of single-sign-out
    We want signing out of account-manager to sign out of the
    finder-frontend, and vice versa, for this initial experiment. This is
    a bit unusual for a federated accounts system like this, but for now
    we are presenting the GOV.UK account as more of a Transition Checker
    account.  We will work on the larger design problem of making users
    understand that the account is a distinct thing later.
    
    Here's how it'll work. It works the same on both sides, so I'll just
    call the two endpoints "logoutA" and "logoutB".
    
    1. User visits logoutA
    2. User is 302-redirected to logoutB?continue=1
    3. User's B session cookie is deleted
    4. User is redirected to logoutA?done=1
    5. User's A session cookie is deleted
    6. The post-logout screen or redirect is shown
    
    Redirecting through every signed-in service won't scale beyond 1
    service, but for this first experiment, we do only have 1.

--- spec/requests/logout_spec.rb
+++ spec/requests/logout_spec.rb
@@ -6,9 +6,15 @@ RSpec.describe "logout" do
   before { sign_in(user) }
 
   describe "GET" do
-    it "refreshes the salt" do
+    it "refreshes the salt on ?continue" do
       old_salt = user.authenticatable_salt
-      get destroy_user_session_path
+      get destroy_user_session_path(continue: 1)
+      expect(user.reload.authenticatable_salt).to_not eq(old_salt)
+    end
+
+    it "refreshes the salt on ?done" do
+      old_salt = user.authenticatable_salt
+      get destroy_user_session_path(done: 1)
       expect(user.reload.authenticatable_salt).to_not eq(old_salt)
     end
   end
