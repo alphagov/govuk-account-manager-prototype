e33d90c78c36788ab7fac4e0a241b97935db82bb
Michael Walker <mike@barrucadu.co.uk>
2020-10-30 11:46:01 +0000

    Send a different email to confirm a change of address

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,4 +1,13 @@
 class DeviseMailer < Devise::Mailer
+  def confirmation_instructions(record, token, opts = {})
+    @token = token
+    if record.try(:unconfirmed_email?)
+      devise_mail(record, :change_confirmation_instructions, opts)
+    else
+      devise_mail(record, :confirmation_instructions, opts)
+    end
+  end
+
   def email_changed(record, opts = {})
     if record.try(:unconfirmed_email?)
       devise_mail(record, :email_changing, opts)
