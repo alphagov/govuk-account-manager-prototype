14340cfbb889242a53e548026ee83375465c7cb2
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-11-03 11:26:03 +0000

    Remove the jquery
    - this might need a window.ready but currently unable to test properly due to another problem

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -5,17 +5,15 @@ window.GOVUK.analyticsVars.gaProperty = "UA-26179049-28"
 window.GOVUK.analyticsVars.gaPropertyCrossDomain = "UA-145652997-1"
 window.GOVUK.analyticsVars.linkedDomains = ['www.gov.uk']
 
-$(document).ready(function() {
-  if (typeof window.GOVUK.analyticsInit !== 'undefined') {
-    window.GOVUK.analyticsInit()
+if (typeof window.GOVUK.analyticsInit !== 'undefined') {
+  window.GOVUK.analyticsInit()
 
-    if (window.GOVUK.cookie('cookies_preferences_set') && window.GOVUK.cookie('cookies_policy')) {
-      var currentConsentCookie = JSON.parse(window.GOVUK.cookie('cookies_policy'))
-      if (currentConsentCookie.usage === true) {
-        $('input[name=cookie_consent][value=yes]').prop('checked', true)
-      } else {
-        $('input[name=cookie_consent][value=no]').prop('checked', true)
-      }
+  if (window.GOVUK.cookie('cookies_preferences_set') && window.GOVUK.cookie('cookies_policy')) {
+    var currentConsentCookie = JSON.parse(window.GOVUK.cookie('cookies_policy'))
+    if (currentConsentCookie.usage === true) {
+      $('input[name=cookie_consent][value=yes]').prop('checked', true)
+    } else {
+      $('input[name=cookie_consent][value=no]').prop('checked', true)
     }
   }
-})
+}
