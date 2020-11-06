f4490136b94b977a282ae2855732a3bd9fbc598d
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-11-06 14:17:54 +0000

    Do not allow Sidekiq to retry jobs
    Sidekiq is currently retrying all jobs and ignoring the ActiveJob
    `retry_on` and `discard_on` options.
    
    By setting Sidekiq's retry to false, this will allow us to fall back to
    the ActiveJob retry/discard options.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationJob < ActiveJob::Base
-  # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
-
-  # Most jobs are safe to ignore if the underlying records are no longer available
-  # discard_on ActiveJob::DeserializationError
+  sidekiq_options retry: false
+  retry_on StandardError, wait: :exponentially_longer, attempts: 5
 end
