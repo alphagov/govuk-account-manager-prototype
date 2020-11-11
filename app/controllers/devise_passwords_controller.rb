fa8154533a6f6aeb2d8e689c518af9fde71a60db
Michael Walker <mike@barrucadu.co.uk>
2020-11-11 14:00:27 +0000

    Remove POST /resource/password

--- app/controllers/devise_passwords_controller.rb
+++ app/controllers/devise_passwords_controller.rb
@@ -1,16 +1,4 @@
 class DevisePasswordsController < Devise::PasswordsController
-  # POST /resource/password
-  def create
-    self.resource = resource_class.send_reset_password_instructions(resource_params)
-    yield resource if block_given?
-
-    if successfully_sent?(resource)
-      respond_with({}, location: after_sending_reset_password_instructions_path_for(resource.email))
-    else
-      respond_with(resource)
-    end
-  end
-
   # PUT /resource/password
   def update
     super do |resource|
@@ -25,7 +13,7 @@ class DevisePasswordsController < Devise::PasswordsController
 protected
 
   # The path used after sending reset password instructions
-  def after_sending_reset_password_instructions_path_for(resource_email)
-    reset_password_sent_path(email: resource_email) if is_navigational_format?
+  def after_sending_reset_password_instructions_path_for(_resource_name)
+    reset_password_sent_path(email: resource.email) if is_navigational_format?
   end
 end
