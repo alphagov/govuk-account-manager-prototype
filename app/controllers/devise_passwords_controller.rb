b38fbeb81cf7d21c9c3d57b1f11b2ba3884dc21b
Michael Walker <mike@barrucadu.co.uk>
2020-11-11 13:54:26 +0000

    Reduce PUT /resource/password code duplication

--- app/controllers/devise_passwords_controller.rb
+++ app/controllers/devise_passwords_controller.rb
@@ -13,24 +13,8 @@ class DevisePasswordsController < Devise::PasswordsController
 
   # PUT /resource/password
   def update
-    self.resource = resource_class.reset_password_by_token(resource_params)
-    yield resource if block_given?
-
-    if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      if Devise.sign_in_after_reset_password
-        flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-        set_flash_message!(:notice, flash_message)
-        resource.after_database_authentication
-        sign_in(resource_name, resource)
-      else
-        set_flash_message!(:notice, :updated_not_active)
-      end
-      SecurityActivity.change_password!(resource, request.remote_ip)
-      respond_with resource, location: after_resetting_password_path_for(resource)
-    else
-      set_minimum_password_length
-      respond_with resource
+    super do |resource|
+      SecurityActivity.change_password!(resource, request.remote_ip) if resource.errors.empty?
     end
   end
 
