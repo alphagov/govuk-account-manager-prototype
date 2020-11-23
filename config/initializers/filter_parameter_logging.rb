aecc70984022f0326cfe6a7a06cea411bf39f5ea
huw <huwd@users.noreply.github.com>
2020-11-23 17:12:32 +0000

    Revert "Format logs to JSON"

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,14 +3,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += %i[
-  _ga
-  code
-  email
-  jwt
-  login_state_id
-  password
-  phone
-  registration_state_id
-  reset_password_token
-]
+Rails.application.config.filter_parameters += [:password]
