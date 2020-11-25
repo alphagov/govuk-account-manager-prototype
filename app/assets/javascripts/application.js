de3bfd9a00aaddba8517d0bacb63132c4c2fbec0
Andy Sellick <andy.sellick@digital.cabinet-office.gov.uk>
2020-11-25 17:20:56 +0000

    Add missing component assets
    - some of the styles/print styles/javascripts for some of the components we're using weren't included in the application, a recent audit uncovered

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,9 @@
 //= require govuk_publishing_components/components/button
 //= require govuk_publishing_components/components/checkboxes
 //= require govuk_publishing_components/components/cookie-banner
+//= require govuk_publishing_components/components/details
 //= require govuk_publishing_components/components/error-summary
+//= require govuk_publishing_components/components/govspeak
 //= require govuk_publishing_components/components/radio
 
 //= require ./analytics-track-form
