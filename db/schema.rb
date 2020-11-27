f5f4276c9a020d37eeddd26aa788f78bd1cb9592
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-11-27 13:58:09 +0000

    Add password_ok column to LoginState

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_103355) do
+ActiveRecord::Schema.define(version: 2020_11_27_135533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_103355) do
     t.datetime "created_at", null: false
     t.bigint "user_id", null: false
     t.string "redirect_path", null: false
+    t.boolean "password_ok", default: false, null: false
     t.index ["user_id"], name: "index_login_states_on_user_id"
   end
 
