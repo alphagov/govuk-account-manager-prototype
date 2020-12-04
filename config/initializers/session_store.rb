e5c153cf46244de13b7c20d5dcc56a44be77a0c3
Michael Walker <mike@barrucadu.co.uk>
2020-12-04 11:43:31 +0000

    Set an expiration time on the session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
 GovukAccountManagerPrototype::Application.config.session_store :cookie_store,
                                                                key: "_govuk_account_manager_prototype_session",
+                                                               expire_after: 30.minutes,
                                                                same_site: :lax,
                                                                secure: Rails.env.production?
