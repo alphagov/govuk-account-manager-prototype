efba20639022c4f20ff8e688043edf73ce220b4c
Michael Walker <mike@barrucadu.co.uk>
2020-12-04 10:51:32 +0000

    Revert "Set SameSite=Strict for the session cookie"
    This reverts commit 33ae65835f5af4db37a1ec55e65aae19357baa70.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 GovukAccountManagerPrototype::Application.config.session_store :cookie_store,
                                                                key: "_govuk_account_manager_prototype_session",
-                                                               same_site: :strict,
                                                                secure: Rails.env.production?
