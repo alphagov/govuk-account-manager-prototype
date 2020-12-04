86d7df86a10a95c0ebd6d48a2e0e3fe22f3d0144
Michael Walker <mike@barrucadu.co.uk>
2020-12-04 10:51:40 +0000

    Revert "Set 'secure' flag for session cookie in production"
    This reverts commit aee0aec3b01ba52d76b88a9d1a8403385a3492b5.

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-GovukAccountManagerPrototype::Application.config.session_store :cookie_store,
-                                                               key: "_govuk_account_manager_prototype_session",
-                                                               secure: Rails.env.production?
