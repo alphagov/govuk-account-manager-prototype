alphagov/govuk-account-manager-prototype

commit 55a8baa07819281390727cb4826df8424efb8cec

Remove session[:jwt_id] from the session after creating a state

app/controllers/devise_registration_controller.rb
@@ -45,6 +45,7 @@ class DeviseRegistrationController < Devise::RegistrationsController
         MultiFactorAuth.generate_and_send_code(@registration_state) if MultiFactorAuth.is_enabled?
       end
       session[:registration_state_id] = @registration_state.id
+      session.delete(:jwt_id)
       redirect_to url_for_state
     end
   end

app/controllers/devise_sessions_controller.rb
@@ -31,6 +31,7 @@ class DeviseSessionsController < Devise::SessionsController
 
       @login_state_id = login_state.id
       session[:login_state_id] = login_state.id
+      session.delete(:jwt_id)
 
       if request.env["warden.mfa.required"]
         MultiFactorAuth.generate_and_send_code(resource)

commit 8f4643d98e6f963702f7601b24efb822a1926bdb

Add test that JWT is kept when moving from registration to login
This was unintentionally broken in 55a8baa.

spec/requests/jwt_login_spec.rb
@@ -46,9 +46,17 @@ RSpec.describe "JWT log in" do
     expect(response).to redirect_to(jwt_post_login_oauth.delete_prefix(Rails.application.config.redirect_base_url))
   end
 
-  context "the user is already logged in" do
-    let(:user) { FactoryBot.create(:user) }
+  it "preserves the JWT if the user comes from registration" do
+    post welcome_path, params: { "jwt" => jwt }
 
+    post new_user_registration_start_path, params: { "user[email]" => "email-for-this-test@example.com", "user[password]" => "password" }
+    expect(RegistrationState.count).to be(1)
+
+    post new_user_session_path, params: { "user[email]" => user.email, "user[password]" => user.password }
+    expect(response).to redirect_to(jwt_post_login_oauth.delete_prefix(Rails.application.config.redirect_base_url))
+  end
+
+  context "the user is already logged in" do
     before do
       sign_in(user)
     end