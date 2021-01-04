e7e564f6dbd3b034b788842d78fdf8a5bf77ab53
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-01-04 11:51:41 +0000

    Add rake task to report on commonly used passwords
    We have recently implemented a block on new users using a password on
    the NCSC's commonly used password list [1].
    
    However we believe existing users may have already used these passwords.
    This task will report how many users this affects, so we can plan on how
    to ask them to change their password.
    
    Since the task is slow and could be terminated by a deployment, it is
    run as a Sidekiq job in a separate queue (with the lowest priority), so
    as not to block any other queued tasks. If re-run, it will only check
    users who do not have an existing check result stored.
    
    1: https://github.com/alphagov/govuk-account-manager-prototype/pull/495

---
+++ spec/jobs/user_banned_password_check_job_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe UserBannedPasswordCheckJob do
+  let!(:user) { FactoryBot.create(:user) }
+
+  before(:all) do
+    BannedPassword.import_list(%w[banned-password])
+  end
+
+  context "user has a banned password" do
+    before do
+      user.update_attribute(:password, "banned-password") # rubocop:disable Rails/SkipsModelValidations
+    end
+
+    it "marks their password as banned" do
+      UserBannedPasswordCheckJob.perform_now(user.id)
+
+      expect(user.reload.banned_password_match).to be true
+    end
+  end
+
+  context "user does not have a banned password" do
+    before do
+      user.update_attribute(:password, "not-a-banned-password") # rubocop:disable Rails/SkipsModelValidations
+    end
+
+    it "marks their password as banned" do
+      UserBannedPasswordCheckJob.perform_now(user.id)
+
+      expect(user.reload.banned_password_match).to be false
+    end
+  end
+end
