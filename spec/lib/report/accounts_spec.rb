8409356bb03242b6752bd7cf690942c39c41a050
Michael Walker <mike@barrucadu.co.uk>
2021-01-06 13:39:31 +0000

    Add reporting task for accounts

---
+++ spec/lib/report/accounts_spec.rb
@@ -0,0 +1,28 @@
+RSpec.describe Report::Accounts do
+  let(:report) { described_class.report(user_id_pepper: "pepper") }
+
+  context "with no users" do
+    it "returns []" do
+      expect(report).to eq([])
+    end
+  end
+
+  context "with users" do
+    it "finds all the users" do
+      u1 = FactoryBot.create(:user, email: "foo@example.com", cookie_consent: false, feedback_consent: true)
+      u2 = FactoryBot.create(:user, email: "bar@example.com", cookie_consent: true, feedback_consent: false)
+
+      expect(report.count).to eq(2)
+      expect(report.map { |e| e[:user_id] }.uniq.count).to eq(2)
+      expect(report.map { |e| e[:registration_timestamp] }).to eq([u1, u2].map(&:created_at))
+      expect(report.map { |e| e[:cookie_consent] }).to eq([u1, u2].map(&:cookie_consent))
+      expect(report.map { |e| e[:feedback_consent] }).to eq([u1, u2].map(&:feedback_consent))
+    end
+
+    it "hashes the user IDs" do
+      u = FactoryBot.create(:user, email: "foo@example.com", cookie_consent: false, feedback_consent: true)
+
+      expect(report[0][:user_id]).to_not eq(u.id)
+    end
+  end
+end
