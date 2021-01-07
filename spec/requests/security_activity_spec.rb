53b50ffcbf04b067b701e9ef329ce937d9368760
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2021-01-07 13:24:02 +0000

    Refactor existing security expect event method
    The method assumed that the user would always be the factory at the top
    of the test file. This works for security events affecting an existing
    user.
    
    However we want to add a test that checks for a security event on user
    creation. So this adds the optional ability to pass a user explcitly to
    be matched in the security events.
    
    The number of optional parameters was getting quite long for the method
    so I switched it for a splat'd options hash.
    
    Defaults are then merged in from a new method below.

--- spec/requests/security_activity_spec.rb
+++ spec/requests/security_activity_spec.rb
@@ -23,28 +23,28 @@ RSpec.describe "security activities" do
       post new_user_session_path, params: { "user[email]" => user.email, "user[password]" => user.password }
       post user_session_phone_verify_path, params: { "phone_code" => user.reload.phone_code }
 
-      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_SUCCESS, factor: :sms
+      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_SUCCESS, { factor: :sms }
     end
 
     it "records ADDITIONAL_FACTOR_VERIFICATION_SUCCESS event with additional analytics data from confirmation email" do
       post new_user_session_path, params: { "user[email]" => user.email, "user[password]" => user.password }
       post user_session_phone_verify_path, params: { "phone_code" => user.reload.phone_code, "from_confirmation_email" => true }
 
-      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_SUCCESS, analytics: "from_confirmation_email"
+      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_SUCCESS, { analytics: "from_confirmation_email" }
     end
 
     it "records ADDITIONAL_FACTOR_VERIFICATION_FAILURE events" do
       post new_user_session_path, params: { "user[email]" => user.email, "user[password]" => user.password }
       post user_session_phone_verify_path, params: { "phone_code" => "incorrect" }
 
-      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_FAILURE, factor: :sms
+      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_FAILURE, { factor: :sms }
     end
 
     it "records ADDITIONAL_FACTOR_VERIFICATION_FAILURE event with additional analytics data from confirmation email" do
       post new_user_session_path, params: { "user[email]" => user.email, "user[password]" => user.password }
       post user_session_phone_verify_path, params: { "phone_code" => "incorrect", "from_confirmation_email" => true }
 
-      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_FAILURE, analytics: "from_confirmation_email"
+      expect_event SecurityActivity::ADDITIONAL_FACTOR_VERIFICATION_FAILURE, { analytics: "from_confirmation_email" }
     end
   end
 
@@ -70,7 +70,7 @@ RSpec.describe "security activities" do
   it "records LOGIN_FAILURE event with additional analytics data from confirmation email" do
     post new_user_session_path, params: { "user[email]" => user.email, "user[password]" => "incorrect", "from_confirmation_email" => true }
 
-    expect_event SecurityActivity::LOGIN_FAILURE, analytics: "from_confirmation_email"
+    expect_event SecurityActivity::LOGIN_FAILURE, { analytics: "from_confirmation_email" }
   end
 
   it "records PASSWORD_RESET_REQUEST events" do
@@ -106,7 +106,7 @@ RSpec.describe "security activities" do
       it "records LOGIN_SUCCESS events for OAuth authorizations" do
         get authorization_endpoint_url(client: application, scope: "openid")
 
-        expect_event SecurityActivity::LOGIN_SUCCESS, application: application
+        expect_event SecurityActivity::LOGIN_SUCCESS, { application: application }
       end
     end
 
@@ -117,7 +117,7 @@ RSpec.describe "security activities" do
         "user[current_password]" => user.password,
       }
 
-      expect_event SecurityActivity::EMAIL_CHANGE_REQUESTED, notes: "from #{user.email} to #{user.reload.unconfirmed_email}"
+      expect_event SecurityActivity::EMAIL_CHANGE_REQUESTED, { notes: "from #{user.email} to #{user.reload.unconfirmed_email}" }
     end
 
     context "with MFA enabled" do
@@ -132,7 +132,7 @@ RSpec.describe "security activities" do
         }
         post edit_user_registration_phone_verify_path, params: { "phone_code" => user.phone_code }
 
-        expect_event SecurityActivity::PHONE_CHANGED, notes: "from #{old_phone} to #{user.reload.phone}"
+        expect_event SecurityActivity::PHONE_CHANGED, { notes: "from #{old_phone} to #{user.reload.phone}" }
       end
     end
 
@@ -154,12 +154,13 @@ RSpec.describe "security activities" do
     expect_event SecurityActivity::EMAIL_CHANGED, notes: "to #{user.email}"
   end
 
-  def expect_event(event, application: nil, factor: nil, notes: nil, analytics: nil)
-    events = user.security_activities.of_type(event)
-    events = events.where(oauth_application_id: application.id) if application
-    events = events.where(factor: factor) if factor
-    events = events.where(notes: notes) if notes
-    events = events.where(analytics: analytics) if analytics
+  def expect_event(event, options = {})
+    event_user = options[:user] || user
+    events = event_user.security_activities.of_type(event)
+    events = events.where(oauth_application_id: options[:application].id) if options[:application]
+    events = events.where(factor: options[:factor]) if options[:factor]
+    events = events.where(notes: options[:notes]) if options[:notes]
+    events = events.where(analytics: options[:analytics]) if options[:analytics]
 
     expect(events.count).to_not eq(0)
   end
