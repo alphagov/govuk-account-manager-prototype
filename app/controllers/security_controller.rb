930d54842d082fe1b6507cb44ba0a7bceaca95d5
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2021-01-19 11:24:53 +0000

    Paginate security events
    As users utilise their accounts, the number of security events will
    increase. Therefore we are paginating the events page so users can more
    clearly see the events.
    
    Co-authored-by: Dana Cotoran <dana.cotoran@gmail.com>

--- app/controllers/security_controller.rb
+++ app/controllers/security_controller.rb
@@ -11,6 +11,34 @@ class SecurityController < ApplicationController
 
   def report; end
 
+  def paginated_activity
+    page_number = params[:page_number].to_i
+    user_activity = current_user.security_activities.show_on_security_page.order(created_at: :desc).page(page_number)
+
+    @activity = user_activity
+    @activity_with_country = user_activity.map(&:fill_missing_country)
+
+    unless user_activity.out_of_range?
+      @page_navigation = {}
+
+      unless user_activity.first_page?
+        @page_navigation[:previous_page] = {
+          url: account_security_paginated_path(page_number: @activity.prev_page),
+          title: t("account.security.page_numbering_previous"),
+          label: t("account.security.page_numbering_navigation", target_page: @activity.prev_page, total_pages: @activity.total_pages),
+        }
+      end
+
+      unless user_activity.last_page?
+        @page_navigation[:next_page] = {
+          url: account_security_paginated_path(page_number: @activity.next_page),
+          title: t("account.security.page_numbering_next"),
+          label: t("account.security.page_numbering_navigation", target_page: @activity.next_page, total_pages: @activity.total_pages),
+        }
+      end
+    end
+  end
+
 private
 
   def dedup_nearby(activities)
--- spec/requests/security_activity_spec.rb
+++ spec/requests/security_activity_spec.rb
@@ -182,6 +182,125 @@ RSpec.describe "security activities" do
     expect_event_on_security_page SecurityActivity::EMAIL_CHANGED
   end
 
+  context "pagination" do
+    context "when there is one page of events" do
+      before do
+        5.times do
+          SecurityActivity.create!(
+            event_type: SecurityActivity::LOGIN_SUCCESS.id,
+            user_id: user.id,
+            ip_address: "1.1.1.1",
+            ip_address_country: "GB",
+          )
+        end
+
+        sign_in user
+      end
+
+      it "the first page has 5 events" do
+        expect_event_on_paginated_security_page(SecurityActivity::LOGIN_SUCCESS, page_number: 1, count: 5)
+      end
+
+      it "the first page does not have a previous link and does not have a next link" do
+        get account_security_paginated_path(page_number: 1)
+
+        expect(response.body).not_to have_content(I18n.t("account.security.page_numbering_previous"))
+        expect(response.body).not_to have_content(I18n.t("account.security.page_numbering_next"))
+      end
+    end
+
+    context "when there are two pages of events" do
+      before do
+        15.times do
+          SecurityActivity.create!(
+            event_type: SecurityActivity::LOGIN_SUCCESS.id,
+            user_id: user.id,
+            ip_address: "1.1.1.1",
+            ip_address_country: "GB",
+          )
+        end
+
+        sign_in user
+      end
+
+      it "the first page has 10 events" do
+        expect_event_on_paginated_security_page(SecurityActivity::LOGIN_SUCCESS, page_number: 1, count: 10)
+      end
+
+      it "the second page has 5 events" do
+        expect_event_on_paginated_security_page(SecurityActivity::LOGIN_SUCCESS, page_number: 2, count: 5)
+      end
+
+      it "the first page does not have a previous link and has a next link" do
+        get account_security_paginated_path(page_number: 1)
+
+        expect(response.body).not_to have_content(I18n.t("account.security.page_numbering_previous"))
+        expect(response.body).to have_content(I18n.t("account.security.page_numbering_next"))
+      end
+
+      it "the second page has a previous link and does not have a next link" do
+        get account_security_paginated_path(page_number: 2)
+
+        expect(response.body).to have_content(I18n.t("account.security.page_numbering_previous"))
+        expect(response.body).not_to have_content(I18n.t("account.security.page_numbering_next"))
+      end
+    end
+
+    context "when there are three pages of events" do
+      before do
+        25.times do
+          SecurityActivity.create!(
+            event_type: SecurityActivity::LOGIN_SUCCESS.id,
+            user_id: user.id,
+            ip_address: "1.1.1.1",
+            ip_address_country: "GB",
+          )
+        end
+
+        sign_in user
+      end
+
+      it "the first page has 10 events" do
+        expect_event_on_paginated_security_page(SecurityActivity::LOGIN_SUCCESS, page_number: 1, count: 10)
+      end
+
+      it "the second page has 10 events" do
+        expect_event_on_paginated_security_page(SecurityActivity::LOGIN_SUCCESS, page_number: 2, count: 10)
+      end
+
+      it "the third page has 5 events" do
+        expect_event_on_paginated_security_page(SecurityActivity::LOGIN_SUCCESS, page_number: 3, count: 5)
+      end
+
+      it "the first page does not have a previous link and has a next link" do
+        get account_security_paginated_path(page_number: 1)
+
+        expect(response.body).not_to have_content(I18n.t("account.security.page_numbering_previous"))
+        expect(response.body).to have_content(I18n.t("account.security.page_numbering_next"))
+      end
+
+      it "the second page has a previous and next link" do
+        get account_security_paginated_path(page_number: 2)
+
+        expect(response.body).to have_content(I18n.t("account.security.page_numbering_previous"))
+        expect(response.body).to have_content(I18n.t("account.security.page_numbering_next"))
+      end
+
+      it "the third page has a previous link and does not have a next link" do
+        get account_security_paginated_path(page_number: 3)
+
+        expect(response.body).to have_content(I18n.t("account.security.page_numbering_previous"))
+        expect(response.body).not_to have_content(I18n.t("account.security.page_numbering_next"))
+      end
+
+      it "shows an error if page does not exist" do
+        get account_security_paginated_path(page_number: 4)
+
+        expect(response.body).to have_content(I18n.t("account.security.page_out_of_range"))
+      end
+    end
+  end
+
   def expect_event(event, options = {})
     event_user = options[:user] || user
     events = event_user.security_activities.of_type(event)
@@ -198,5 +317,11 @@ RSpec.describe "security activities" do
     get account_security_path
 
     expect(response.body).to have_content(I18n.t("account.security.event.#{event.name}"))
+  end
+
+  def expect_event_on_paginated_security_page(event, page_number:, count:)
+    get account_security_paginated_path(page_number: page_number)
+
+    expect(response.body).to have_content(I18n.t("account.security.event.#{event.name}"), count: count)
   end
 end
