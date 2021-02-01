0b7286c213ae35a80852b98030bf1efac0f08447
Michael Walker <mike@barrucadu.co.uk>
2021-02-01 13:47:01 +0000

    Retry timed-out attribute service requests
    We're experiencing an AWS outage in which requests to one AZ are
    failing.  Relying on every AZ being working is fragile, and indeed we
    have multiple instances of our apps which (hopefully) are spread
    around.  So if one request fails, try again.

--- spec/lib/remote_user_info_spec.rb
+++ spec/lib/remote_user_info_spec.rb
@@ -11,22 +11,10 @@ RSpec.describe RemoteUserInfo do
     end
   end
 
-  context "the attribute service is down" do
-    before do
-      stub_request(:get, "#{attribute_service_url}/oidc/user_info")
-        .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" })
-        .to_return(status: 500)
-    end
+  context "#user_info" do
+    let(:attributes) { { attribute: "value" } }
 
-    it "returns nil" do
-      expect(described_class.call(user)).to be_nil
-    end
-  end
-
-  context "the attribute service is up" do
     it "calls the attribute service" do
-      attributes = { attribute: "value" }
-
       stub_request(:get, "#{attribute_service_url}/oidc/user_info")
         .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" })
         .to_return(body: attributes.to_json)
@@ -34,16 +22,85 @@ RSpec.describe RemoteUserInfo do
       expect(described_class.call(user)).to eq(attributes)
     end
 
-    context "#update_profile!" do
-      it "calls the attribute service to set the profile attributes" do
-        body = { attributes: { email: user.email.to_json, email_verified: user.confirmed?.to_json } }
-        stub = stub_request(:post, "#{attribute_service_url}/v1/attributes")
+    context "the attribute service is down" do
+      before do
+        stub_request(:get, "#{attribute_service_url}/oidc/user_info")
+          .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" })
+          .to_return(status: 500)
+      end
+
+      it "returns nil" do
+        expect(described_class.call(user)).to be_nil
+      end
+    end
+
+    context "the attribute service sporadically times out" do
+      let(:final_status) { 200 }
+
+      before do
+        stub_request(:get, "#{attribute_service_url}/oidc/user_info")
+          .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" })
+          .to_return(status: 504)
+        stub_request(:get, "#{attribute_service_url}/oidc/user_info")
+          .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" })
+          .to_return(status: 504)
+        stub_request(:get, "#{attribute_service_url}/oidc/user_info")
+          .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" })
+          .to_return(status: final_status, body: attributes.to_json)
+      end
+
+      it "tries 3 times" do
+        expect(described_class.call(user)).to eq(attributes)
+      end
+
+      context "the 3rd attempt fails" do
+        let(:final_status) { 504 }
+
+        it "returns nil" do
+          expect(described_class.call(user)).to be_nil
+        end
+      end
+    end
+  end
+
+  context "#update_profile!" do
+    let(:body) { { attributes: { email: user.email.to_json, email_verified: user.confirmed?.to_json } } }
+
+    it "calls the attribute service to set the profile attributes" do
+      stub = stub_request(:post, "#{attribute_service_url}/v1/attributes")
+        .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" }, body: body)
+        .to_return(status: 200)
+
+      described_class.new(user).update_profile!
+      expect(stub).to have_been_made
+    end
+
+    context "the attribute service sporadically times out" do
+      let(:final_status) { 200 }
+
+      before do
+        stub_request(:post, "#{attribute_service_url}/v1/attributes")
           .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" }, body: body)
-          .to_return(status: 200)
+          .to_return(status: 504)
+        stub_request(:post, "#{attribute_service_url}/v1/attributes")
+          .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" }, body: body)
+          .to_return(status: 504)
+        @stub = stub_request(:post, "#{attribute_service_url}/v1/attributes")
+          .with(headers: { accept: "application/json", authorization: "Bearer #{bearer_token}" }, body: body)
+          .to_return(status: final_status)
+      end
 
+      it "tries 3 times" do
         described_class.new(user).update_profile!
+        expect(@stub).to have_been_made
+      end
 
-        expect(stub).to have_been_made
+      context "the 3rd attempt fails" do
+        let(:final_status) { 504 }
+
+        it "re-throws the exception" do
+          expect { described_class.new(user).update_profile! }.to raise_error(RestClient::GatewayTimeout)
+        end
       end
     end
   end
