4959b633c0736f704f82a213be058d04f4ad0eef
Michael Walker <mike@barrucadu.co.uk>
2021-02-03 14:20:24 +0000

    Add a notice to every account page if the user has a weak password

---
+++ spec/feature/insecure_password_spec.rb
@@ -0,0 +1,28 @@
+RSpec.feature "Insecure Passwords" do
+  let!(:user) { FactoryBot.create(:user) }
+
+  before do
+    allow(Rails.configuration).to receive(:feature_flag_mfa).and_return(true)
+    BannedPassword.import_list([user.password])
+  end
+
+  it "shows the alert in the account manager" do
+    enter_email_address_and_password
+    enter_mfa
+
+    expect(page).to have_text(I18n.t("insecure_password.notice.message"))
+  end
+
+  def enter_email_address_and_password(email: user.email, password: user.password)
+    visit new_user_session_path
+    fill_in "email", with: email
+    fill_in "password", with: password
+    click_on I18n.t("devise.sessions.new.fields.submit.label")
+  end
+
+  def enter_mfa(the_user: user)
+    phone_code = the_user.reload.phone_code
+    fill_in "phone_code", with: phone_code
+    click_on I18n.t("mfa.phone.code.fields.submit.label")
+  end
+end
