2f911799aea8702c0bdaa8e3e27d232a899aa35c
Michael Walker <mike@barrucadu.co.uk>
2021-02-10 10:09:55 +0000

    Suppress "foo is suspended" Zendesk errors
    Some email addresses are suspended.  Maybe spammers?  Trying to submit
    a ticket with a suspended email address raises an exception, which we
    send to Sentry.  We can't do anything about it, and we don't get many
    of them, so just ignore them.

--- app/jobs/zendesk_ticket_job.rb
+++ app/jobs/zendesk_ticket_job.rb
@@ -6,5 +6,7 @@ class ZendeskTicketJob < ApplicationJob
   def perform(ticket_attributes)
     ticket = Zendesk::Ticket.new(ticket_attributes).attributes
     GDS_ZENDESK_CLIENT.ticket.create!(ticket)
+  rescue ZendeskAPI::Error::RecordInvalid => e
+    raise e unless e.to_s.include? "is suspended."
   end
 end
