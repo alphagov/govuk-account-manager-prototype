2a47e2c5d88c05a3280f4432aa913fa3d904df97
Michael Walker <mike@barrucadu.co.uk>
2021-02-17 11:00:16 +0000

    Remove the transition checker cookie consent hack
    This is no longer needed, as the transition checker uses the
    slightly-less-of-a-hack ephemeral state workaround we implemented.
    
    https://github.com/alphagov/finder-frontend/blob/master/app/controllers/sessions_controller.rb#L35-L48

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -58,8 +58,6 @@ RSpec.describe ApplicationHelper do
   end
 
   describe "#service_for" do
-    let(:user) { FactoryBot.create(:user) }
-
     let(:application) do
       FactoryBot.create(
         :oauth_application,
@@ -71,18 +69,18 @@ RSpec.describe ApplicationHelper do
 
     it "extracts the service name using the client_id parameter" do
       url = oauth_authorization_path + "?" + Rack::Utils.build_nested_query(client_id: application.uid)
-      expect(service_for(url, user)[:name]).to eq(application.name)
+      expect(service_for(url)[:name]).to eq(application.name)
     end
 
     it "only produces a service name if the link looks like an OAuth content URL" do
       url = "//nefarious-attempt-to-embed-an-arbitrary-link?" + Rack::Utils.build_nested_query(client_id: application.uid)
-      expect(service_for(url, user)).to be_nil
+      expect(service_for(url)).to be_nil
     end
 
     context "the client_id doesn't match an application" do
       it "returns nil" do
         url = oauth_authorization_path + "?" + Rack::Utils.build_nested_query(client_id: "breadbread")
-        expect(service_for(url, user)).to be_nil
+        expect(service_for(url)).to be_nil
       end
     end
   end
