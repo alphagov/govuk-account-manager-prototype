6cdecfa1d30a61af197221a0f7bc68788018d63c
Michael Walker <mike@barrucadu.co.uk>
2021-02-18 14:12:43 +0000

    Simplify JWT validation logic
    All JWTs now use the `application_id_from_token` path.

--- app/models/jwt.rb
+++ app/models/jwt.rb
@@ -25,15 +25,11 @@ class Jwt < ApplicationRecord
   end
 
   class InvalidJWT < StandardError; end
+  class MissingApplicationId < InvalidJWT; end
+  class ApplicationNotFound < InvalidJWT; end
   class InsufficientScopes < InvalidJWT; end
-  class InvalidScopes < InvalidJWT; end
-  class JWTDecodeError < InvalidJWT; end
-  class KeyNotFound < InvalidJWT; end
-  class MissingFieldKey < InvalidJWT; end
-  class MissingFieldUid < InvalidJWT; end
-  class MissingFieldPostLoginOAuth < InvalidJWT; end
-  class UidNotFound < InvalidJWT; end
   class InvalidOAuthRedirect < InvalidJWT; end
+  class JWTDecodeError < InvalidJWT; end
 
   def destroy_stale_states
     transaction do
@@ -52,32 +48,16 @@ class Jwt < ApplicationRecord
 private
 
   def parse_jwt_token
-    payload, = JWT.decode jwt_payload, nil, false
+    raise MissingApplicationId unless application_id_from_token
 
-    if application_id_from_token
+    begin
       application = Doorkeeper::Application.find(application_id_from_token)
-      signing_key = nil
-      scopes = application.scopes.to_a.map(&:to_sym)
-      post_login_oauth = nil
-    else
-      raise MissingFieldUid unless payload["uid"]
-      raise MissingFieldKey unless payload["key"]
-
-      application = Doorkeeper::Application.by_uid(payload["uid"])
-      raise UidNotFound unless application
-
-      signing_key = ApplicationKey.find([payload["uid"], payload["key"]])
-      payload, = JWT.decode jwt_payload, signing_key.to_key, true, { algorithm: "ES256" }
-
-      scopes = payload.fetch("scopes", []).map(&:to_sym)
-      scopes.each do |scope|
-        raise InvalidScopes unless application.includes_scope?(scope)
-      end
-
-      post_login_oauth = payload["post_login_oauth"]&.delete_prefix(Rails.application.config.redirect_base_url)
-      raise MissingFieldPostLoginOAuth unless post_login_oauth
-      raise InvalidOAuthRedirect unless post_login_oauth.starts_with? "/oauth/authorize"
+    rescue ActiveRecord::RecordNotFound
+      raise ApplicationNotFound
     end
+
+    payload, = JWT.decode jwt_payload, nil, false
+    scopes = application.scopes.to_a.map(&:to_sym)
 
     attributes = payload.fetch("attributes", {}).transform_keys(&:to_sym)
     attributes.each_key do |attribute|
@@ -92,15 +72,11 @@ private
 
     self.jwt_payload = {
       application: application,
-      signing_key: signing_key,
       scopes: scopes,
       attributes: attributes,
-      post_login_oauth: post_login_oauth,
       post_register_oauth: post_register_oauth,
     }
   rescue JWT::DecodeError
     raise JWTDecodeError
-  rescue ActiveRecord::RecordNotFound
-    raise KeyNotFound
   end
 end
--- spec/models/jwt_spec.rb
+++ spec/models/jwt_spec.rb
@@ -8,135 +8,51 @@ RSpec.describe Jwt do
       :oauth_application,
       name: "name",
       redirect_uri: "http://localhost",
-      scopes: %i[test_scope_write],
+      scopes: application_scopes,
     )
   end
 
+  let(:application_scopes) { %i[test_scope_write] }
+
+  let(:access_token) { FactoryBot.create(:oauth_access_token, application_id: application.id) }
+
+  let(:jwt_payload) do
+    {
+      attributes: jwt_attributes,
+      post_register_oauth: jwt_post_register_oauth,
+    }.compact
+  end
+
+  let(:jwt_attributes) { { test: "value" } }
+  let(:jwt_post_register_oauth) { "#{Rails.application.config.redirect_base_url}/oauth/authorize?some-other-query-string" }
+  let(:jwt_application_id) { application.id }
+
+  let(:jwt) { Jwt.create!(jwt_payload: JWT.encode(jwt_payload, nil, "none"), application_id_from_token: jwt_application_id) }
+
   context "#jwt_payload" do
-    let(:private_key) do
-      private_key = OpenSSL::PKey::EC.new "prime256v1"
-      private_key.generate_key
-    end
-
-    let(:public_key) { OpenSSL::PKey::EC.new private_key }
-
-    let(:application_key) do
-      ApplicationKey.create!(
-        application_uid: application.uid,
-        key_id: SecureRandom.uuid,
-        pem: public_key.to_pem,
-      )
-    end
-
-    let(:jwt_uid) { application.uid }
-    let(:jwt_key) { application_key.key_id }
-    let(:jwt_scopes) { %i[test_scope_write] }
-    let(:jwt_attributes) { { test: "value" } }
-    let(:jwt_post_login_oauth) { "#{Rails.application.config.redirect_base_url}/oauth/authorize?some-query-string" }
-    let(:jwt_post_register_oauth) { "#{Rails.application.config.redirect_base_url}/oauth/authorize?some-other-query-string" }
-    let(:jwt_signing_key) { private_key }
-
-    let(:jwt) do
-      payload = {
-        uid: jwt_uid,
-        key: jwt_key,
-        scopes: jwt_scopes,
-        attributes: jwt_attributes,
-        post_register_oauth: jwt_post_register_oauth,
-        post_login_oauth: jwt_post_login_oauth,
-      }.compact
-      JWT.encode payload.compact, jwt_signing_key, "ES256"
-    end
-
     it "accepts" do
-      payload = Jwt.create!(jwt_payload: jwt).jwt_payload.deep_symbolize_keys
-      expect(payload).to include(:application, :signing_key, :post_login_oauth, :post_register_oauth, scopes: jwt_scopes.map(&:to_s), attributes: jwt_attributes)
-      expect(payload[:post_register_oauth]).to eq(jwt_post_register_oauth.delete_prefix(Rails.application.config.redirect_base_url))
-      expect(payload[:post_login_oauth]).to eq(jwt_post_login_oauth.delete_prefix(Rails.application.config.redirect_base_url))
-      expect(Doorkeeper::Application.find(payload.dig(:application, :id)).uid).to eq(jwt_uid)
-      expect(payload.dig(:signing_key, :pem)).to eq(public_key.to_pem)
-    end
-
-    context "the JWT came from the API, with an access token" do
-      let(:application_with_token) do
-        FactoryBot.create(
-          :oauth_application,
-          name: "other name",
-          redirect_uri: "http://localhost",
-          scopes: %i[test_scope_write],
-        )
-      end
-
-      let(:access_token) { FactoryBot.create(:oauth_access_token, application_id: application_with_token.id) }
-
-      let(:jwt_signing_key) do
-        private_key = OpenSSL::PKey::EC.new "prime256v1"
-        private_key.generate_key
-      end
-
-      it "skips the crypto and takes the uid from the access token, not the JWT" do
-        jwt_payload = Jwt.create!(jwt_payload: jwt, application_id_from_token: access_token.application_id).jwt_payload.deep_symbolize_keys
-        expect(jwt_payload[:application][:id]).to eq(application_with_token.id)
-        expect(jwt_payload[:signing_key]).to be_nil
-      end
-    end
-
-    context "the JWT is missing a UID" do
-      let(:jwt_uid) { nil }
-
-      it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::MissingFieldUid)
-      end
-    end
-
-    context "the JWT is missing a key ID" do
-      let(:jwt_key) { nil }
-
-      it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::MissingFieldKey)
-      end
+      payload = jwt.jwt_payload.deep_symbolize_keys
+      expect(payload).to match({
+        application: hash_including(id: application.id),
+        scopes: application.scopes.to_a,
+        attributes: jwt_attributes,
+        post_register_oauth: jwt_post_register_oauth.delete_prefix(Rails.application.config.redirect_base_url),
+      })
     end
 
     context "the JWT references a missing application" do
-      let(:jwt_uid) { "breadbread" }
+      let(:jwt_application_id) { "breadbread" }
 
       it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::UidNotFound)
+        expect { jwt }.to raise_error(Jwt::ApplicationNotFound)
       end
     end
 
-    context "the JWT references a missing key" do
-      let(:jwt_key) { "breadbread" }
+    context "the JWT tries to write to an attribute not covered by the application scopes" do
+      let(:application_scopes) { [] }
 
       it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::KeyNotFound)
-      end
-    end
-
-    context "the JWT has been signed with the wrong key" do
-      let(:jwt_signing_key) do
-        private_key = OpenSSL::PKey::EC.new "prime256v1"
-        private_key.generate_key
-      end
-
-      it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::JWTDecodeError)
-      end
-    end
-
-    context "the JWT asks for scopes the application doesn't have" do
-      let(:jwt_scopes) { %i[account_manager_access] }
-
-      it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::InvalidScopes)
-      end
-    end
-
-    context "the JWT tries to write to an attribute without requesting the scope" do
-      let(:jwt_scopes) { [] }
-
-      it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::InsufficientScopes)
+        expect { jwt }.to raise_error(Jwt::InsufficientScopes)
       end
     end
 
@@ -144,15 +60,7 @@ RSpec.describe Jwt do
       let(:jwt_attributes) { { foo: "bar" } }
 
       it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::InsufficientScopes)
-      end
-    end
-
-    context "the JWT is missing the post-login redirect" do
-      let(:jwt_post_login_oauth) { nil }
-
-      it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::MissingFieldPostLoginOAuth)
+        expect { jwt }.to raise_error(Jwt::InsufficientScopes)
       end
     end
 
@@ -160,15 +68,15 @@ RSpec.describe Jwt do
       let(:jwt_post_register_oauth) { nil }
 
       it "accepts" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to_not raise_error
+        expect { jwt }.to_not raise_error
       end
     end
 
     context "the JWT has a bad redirect" do
-      let(:jwt_post_login_oauth) { "https://www.example.com" }
+      let(:jwt_post_register_oauth) { "https://www.example.com" }
 
       it "rejects" do
-        expect { Jwt.create!(jwt_payload: jwt) }.to raise_error(Jwt::InvalidOAuthRedirect)
+        expect { jwt }.to raise_error(Jwt::InvalidOAuthRedirect)
       end
     end
   end
