5d423e35b1808198dc74e13eca2ca8be359ddba7
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2021-02-19 11:51:34 +0000

    TOfixup: auth tests

--- spec/feature/webauthn/authenticate_key_spec.rb
+++ spec/feature/webauthn/authenticate_key_spec.rb
@@ -1,5 +1,5 @@
 RSpec.feature "Authenticate with a new a webauthn key" do
-  secnario do
+  scenario do
     # We had some difficulty getting these to work. The JS will trigger a web browser API which will present the user with a modal box
     # that they can use to authenticate from this point on. However capybara does not appear to have access to that API, and we would need to
     # find a way of stubbing the input from a yubikey.
