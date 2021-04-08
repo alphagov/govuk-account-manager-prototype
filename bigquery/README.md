7227016a352919fd47ee23ae4ed93b41a27b078d
Michael Walker <mike@barrucadu.co.uk>
2021-04-08 10:08:41 +0100

    Add BigQuery table schemas
    These shouldn't just be hanging around on my machine, so I've added
    them here in case we need them again.

---
+++ bigquery/README.md
@@ -0,0 +1,4 @@
+BigQuery Table Schemas
+======================
+
+If we need to recreate the tables, these schemas are the thing to use.
---
+++ bigquery/events.pp
@@ -0,0 +1,17 @@
+[
+   {
+      "name" : "user_id",
+      "mode" : "REQUIRED",
+      "type" : "STRING"
+   },
+   {
+      "mode" : "REQUIRED",
+      "name" : "login_timestamp",
+      "type" : "TIMESTAMP"
+   },
+   {
+      "type" : "STRING",
+      "name" : "login_type",
+      "mode" : "REQUIRED"
+   }
+]
