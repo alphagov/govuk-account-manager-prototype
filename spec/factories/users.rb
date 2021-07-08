43b88444ed34cc99fa71dddf159db97323a4b8d1
Michael Walker <mike@barrucadu.co.uk>
2021-07-08 09:54:17 +0100

    Add an endpoint to migrate users to account-api
    We'll add a rake task to account-api to call this and page through the
    results, importing all the transition checker state & subscriptions.
    
    Then we can:
    
    - mark the transition checker state attribute as "local" in
    account-api,
    - remove the attribute from the attribute service,
    - remove the subscription management logic from the account manager.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "email@example.com" }
+    sequence(:email) { |n| "email-#{n}@example.com" }
     password { "abcd1234" }
     phone { "+447958123456" }
 
